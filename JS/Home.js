572993f2d5bd01f453df77b1c01d3729e44dc895
Your Name <you@example.com>
2021-02-17 22:15:34 -0500

    Adding about me section and the form

--- JS/Home.js
+++ JS/Home.js
@@ -101,3 +101,119 @@ $('.content').on('mouseleave', function(e){
         transform:'translateX(0%)'
     })
 })
+
+
+//Change Icone when form input is focused
+let inputFontSize = $('.contactMeForm input').css('font-size');
+$('.contactMeForm input').focus(function(e){
+    $('.contactMeForm input').prev().addClass('far');
+    $('.contactMeForm input').prev().removeClass('fas');
+    $(e.currentTarget).prev().addClass('fas');
+    $(e.currentTarget).prev().removeClass('far');
+    
+    if($(e.currentTarget).css('border-style').toString() != 'none'){
+        $(e.currentTarget).css({
+            border: 'none',
+            fontSize: inputFontSize.toString(),
+            color: '#1d3557',
+        })
+
+        $(e.currentTarget).val("");
+    }
+})
+
+$('.contactMeForm input').blur(function(){
+    $('.contactMeForm input').prev().addClass('far');
+    $('.contactMeForm input').prev().removeClass('fas');
+
+})
+
+$('.message textarea').focus(function(){
+    $('.contactMeForm input').prev().addClass('far');
+    $('.contactMeForm input').prev().removeClass('fas');
+    $(this).prev().addClass('fas');
+    $(this).prev().removeClass('far');
+
+    if($('.message textarea').css('border-style').toString() != 'none'){
+        $('.message textarea').css({
+            border: 'none',
+            fontSize: inputFontSize.toString(),
+            color: '#1d3557',
+        })
+
+        $('.message textarea').val("");
+    }
+
+})
+
+$('.message textarea').blur(function(){
+    $('.contactMeForm input').prev().addClass('far');
+    $('.contactMeForm input').prev().removeClass('fas');
+    $(this).prev().addClass('far');
+    $(this).prev().removeClass('fas');
+
+})
+
+// Click event for submit button on form
+
+$('#submitButton').on('click',function(){
+    console.log(CheckNameValidity());
+    console.log(CheckEmailValidity());
+    console.log(CheckMessageValidity());
+})
+
+let CheckNameValidity = ()=>{
+    let text = $('#name').val();
+    let letterOnly = /[a-zA-z]+$/;
+    let Invalid = /Invalid/;
+    if(!(letterOnly.test(text)) || Invalid.test(text)){
+        $('#name').css({
+            border: '0.5px solid red',
+            fontSize: '1em',
+            color: 'red'
+        })
+        $('#name').val('Invalid name');
+
+        return false;
+    }
+
+    else return true;
+}
+
+let CheckEmailValidity = () =>{
+    let email = $('#email').val();
+    let validEmail = /\S+@\S+.\S+/;
+    let Invalid = /Invalid/;
+    if(!(validEmail.test(email)) || Invalid.test(email)){
+        $('#email').css({
+            border: '0.5px solid red',
+            fontSize: '1em',
+            color: 'red'
+        })
+
+        $('#email').val('Invalid email');
+
+        return false
+    }
+
+    else return true
+}
+
+let CheckMessageValidity = () =>{
+    let message = $('#message').val();
+    let validMessage = /\S+/;
+    let Invalid = /Invalid/;
+    if(!(validMessage.test(message)) || Invalid.test(message)){
+        $('#message').css({
+            border: '0.5px solid red',
+            fontSize: '1em',
+            color: 'red'
+        })
+
+        $('#message').val('Invalid message');
+
+        return false
+    }
+
+    else return true
+}
